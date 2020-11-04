51e1db2dd271e839d51cf95915963b4743b69dea
martin cailleau <martin.cailleau@gmail.com>
2020-11-04 13:41:26 +0100

    first commit

---
+++ PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    public int playderID;
+    public float speed = 1;
+    public float turnSpeed = 2;
+    public Transform rotationPivot;
+
+    Vector3 move;
+    float turn;
+
+    // Use this for initialization
+    void Start () {
+        move = Vector3.zero;
+        turn = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        turn = Mathf.Lerp(turn, turnSpeed * Input.GetAxis("Horizontal_P" + playderID), 0.1f);
+        transform.RotateAround(rotationPivot.position, Vector3.up, turn);
+        move = Vector3.Lerp(move, Vector3.forward * speed * Input.GetAxis("Vertical_P" + playderID), Time.deltaTime * 1f);
+        move.y -= 5;
+        move.z = Mathf.Clamp01(move.z);
+        GetComponent<CharacterController>().Move(transform.TransformVector(move));
+    }
+}
---
+++ TrailRendererCollider.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class TrailRendererCollider : MonoBehaviour {
+    LineRenderer line;
+    List<Vector3> vertex;
+    RaycastHit hit;
+    public float minVertexDistance;
+    public GameObject[] explosions;
+
+    private bool check = true;
+
+    private float timer = 0;
+	// Use this for initialization
+	void Start () {
+        vertex = new List<Vector3>();  
+        vertex.Add(transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Vector3.Distance(vertex[vertex.Count - 1], transform.position)> minVertexDistance) {
+            vertex.Add(transform.position);
+        }
+
+        timer += Time.deltaTime;
+        if (timer > 0.1f)
+        {   
+            timer = 0;
+            checkCollision();
+        }
+
+
+    }
+
+    void checkCollision() {
+        for (int i=1;i<vertex.Count-1;++i)
+        {
+            if (Physics.Linecast(vertex[i - 1], vertex[i],out hit))
+            {
+                Debug.Log(hit.collider.GetType());
+                if (hit.collider.GetType() != typeof(CharacterController) && check) {
+                    StartCoroutine(destroy(hit.collider.gameObject));
+                    check = false;
+                }
+            }
+        }
+    }
+
+    IEnumerator destroy(GameObject go)
+    {
+        Debug.Log("DESTROY  "+go.name);
+        foreach (GameObject explosion in explosions)
+        {
+            Destroy(Instantiate(explosion, hit.point + (Random.insideUnitSphere * 5) + Vector3.up*2, Quaternion.identity), 3);
+            yield return new WaitForSeconds(0.1f);
+        }
+        Destroy(go);
+        check = true;
+        yield return null;
+    }
+}
